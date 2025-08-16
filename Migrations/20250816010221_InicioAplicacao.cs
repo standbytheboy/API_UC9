da769732176a88eb6d934e43524e0557f158f15e
diego.aasampaio <diego.aasampaio@TIT0676503W11-1>
2025-08-15 22:17:04 -0300

    Adicionar arquivos de projeto.

---
+++ Migrations/20250816010221_InicioAplicacao.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinhaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InicioAplicacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Preco = table.Column<double>(type: "float", nullable: false),
+                    Estoque = table.Column<int>(type: "int", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MinhaAPI.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do produto é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome do produto deve ter no máximo 100 caracteres.")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O preço do produto é obrigatório.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "O preço do produto deve ser maior que zero.")]
+        public double Preco { get; set; }
+        
+        [Required(ErrorMessage = "A quantidade em estoque é obrigatória.")]
+        public int Estoque { get; set; }
+
+        [StringLength(500, ErrorMessage = "A descrição deve ter no máximo 500 caracteres")]
+        public string Descricao { get; set; } = string.Empty;
+
+    }
+}
