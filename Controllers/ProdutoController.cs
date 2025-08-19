12b4c2e18fd684ed8941dd527eff9be13bdc401d
diego-aquila <diego@diegoaquila.com.br>
2025-08-18 22:28:09 -0300

    Criação de Métodos Get, Post e Delete

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MinhaAPI.Data;
+using MinhaAPI.Models;
+
+namespace MinhaAPI.Controllers
+{
+    [ApiController] // Define a rota como Controller de API
+    [Route("api/[controller]")] // api/Produto
+     
+    public class ProdutoController : ControllerBase
+    {
+        private readonly AppDbContext _contextDb;
+
+        public ProdutoController(AppDbContext contextDb)
+        {
+            _contextDb = contextDb;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() { 
+        
+            var produtos = await _contextDb.Produtos.ToListAsync();
+            return Ok(produtos); 
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Produto produto) {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _contextDb.Produtos.Add(produto);
+            await _contextDb.SaveChangesAsync();
+
+            return Ok("Produto criado com sucesso");
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id) {
+
+            var produto = await _contextDb.Produtos.FindAsync(id);
+
+            if (produto == null)
+            {
+                return NotFound($"Produto com o id {id} não foi encontrado");
+
+            }
+
+            _contextDb.Produtos.Remove(produto);
+            await _contextDb.SaveChangesAsync();
+            return Ok("Produto deletado com sucesso");
+        
+        }
+
+    }
+}
